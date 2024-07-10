2385f9f41f6e0a99611c0cc47d91c56779231241
Admin <amanayemonesia@ya.ru>
2024-07-11 00:47:55 +0300

    feat: add repeat password field, add response error listener

--- src/pages/reset-password/reset-password.constants.ts
+++ src/pages/reset-password/reset-password.constants.ts
@@ -2,5 +2,6 @@ import type { Schema } from './reset-password.interface.ts';
 
 export const defaultValues: Schema = {
   password: '',
+  repeatPassword: '',
   token: '',
 };
--- src/pages/reset-password/reset-password.schema.ts
+++ src/pages/reset-password/reset-password.schema.ts
@@ -4,5 +4,9 @@ import { validators } from '@shared/utils';
 
 export const schema = object({
   password: validators.getStringPasswordValidationSchema(),
+  repeatPassword: validators.getStringPasswordValidationSchema(),
   token: validators.getStringRequiredValidationSchema(),
+}).refine(({ password, repeatPassword }) => password === repeatPassword, {
+  message: 'Пароли не совпадают',
+  path: ['repeatPassword'],
 });
