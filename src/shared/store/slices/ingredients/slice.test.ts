04da5546c5758986c9f58d69d57c71b829025b45
Canfus <canfus69@gmail.com>
2024-07-14 13:58:07 +0300

    feat: add more cases

--- src/shared/store/slices/ingredients/slice.test.ts
+++ src/shared/store/slices/ingredients/slice.test.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import '@testing-library/react';
 
 import { endpoints } from '../../../api/queries/queries.constants';
-import { IngredientListResponse } from '../../../api/models';
+import { IngredientListResponse, Ingredient } from '../../../api/models';
 import { store, ingredientsActions } from '../..';
 
 const { dispatch, getState } = store;
@@ -20,6 +20,58 @@ describe('Ingredient testing case', () => {
 
         expect(ingredients.length).toBeGreaterThan(0);
       });
+  });
+
+  it('Should select single ingredient', () => {
+    const { select } = ingredientsActions;
+
+    const bun: Ingredient = {
+      _id: '643d69a5c3f7b9001cfa093c',
+      name: 'Краторная булка N-200i',
+      type: 'bun',
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+      __v: 0,
+    };
+
+    dispatch(select(bun));
+
+    const { selectedIngredient } = getState().ingredientsReducer;
+
+    expect(selectedIngredient).toEqual(bun);
+  });
+
+  it('Should remove selected ingredient', () => {
+    const { select, remove } = ingredientsActions;
+
+    const bun: Ingredient = {
+      _id: '643d69a5c3f7b9001cfa093c',
+      name: 'Краторная булка N-200i',
+      type: 'bun',
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+      __v: 0,
+    };
+
+    dispatch(select(bun));
+
+    dispatch(remove());
+
+    const { selectedIngredient } = getState().ingredientsReducer;
+
+    expect(selectedIngredient).toBeNull();
   });
 });
 
