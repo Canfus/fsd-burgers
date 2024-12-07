61d5501a7ab5883d557418cc5054626a10ee6491
canfus <canfus69@gmail.com>
2024-12-07 22:32:34 +0300

    gh pages

--- src/app/providers/router/router.provider.tsx
+++ src/app/providers/router/router.provider.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import type { ProviderProps } from '../providers.interface';
 
 export const RouterProvider = ({ children }: ProviderProps) => (
-  <BrowserRouter>{children}</BrowserRouter>
+  <HashRouter>{children}</HashRouter>
 );
